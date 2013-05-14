f556859fb4d94d67a835228e0dbc28163f63b637
nigel <nigel_pickard@yahoo.com>
2013-05-14 13:20:32 -0400

    a commit in the new branch

--- src/com/npickard/HelloWorld.java
+++ src/com/npickard/HelloWorld.java
@@ -13,7 +13,7 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello world.... with a change!");
+		System.out.println("Hello world.... with a change -and it now in a branch!");
 	}
 
 }
