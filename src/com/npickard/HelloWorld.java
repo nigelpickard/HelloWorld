57219b8d52fe47ef681fe11c6fae3c9d0a41a5a3
nigel <nigel_pickard@yahoo.com>
2013-05-14 13:21:37 -0400

    a commit in the original master

--- src/com/npickard/HelloWorld.java
+++ src/com/npickard/HelloWorld.java
@@ -13,7 +13,7 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello world.... with a change!");
+		System.out.println("Hello world.... with a change that is not in a branch!");
 	}
 
 }
