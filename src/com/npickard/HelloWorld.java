e342e4087b0c83b698caaaa645d6ebbee473b307
nigel <nigel_pickard@yahoo.com>
2013-05-14 13:27:07 -0400

    changed text

--- src/com/npickard/HelloWorld.java
+++ src/com/npickard/HelloWorld.java
@@ -13,7 +13,7 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello world.... with a change that is not in a branch!");
+		System.out.println("Hello world.... with a change that is not in a branch! It is in the master");
 	}
 
 }
