5ea4420cd99b71c129b1aed98497a5882d7eeeab
nigel <nigel_pickard@yahoo.com>
2013-05-14 13:14:59 -0400

    Initial commit of Helloworld.

---
+++ src/com/npickard/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.npickard;
+
+/**
+ * @author nigel
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world....");
+	}
+
+}
