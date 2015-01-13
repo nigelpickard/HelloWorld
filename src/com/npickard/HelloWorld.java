b325f15db7ac629c0f145401b3dbe7942b80a7cc
npickard <npickard@platformq.com>
2015-01-13 14:01:43 -0500

    Removed references to logger to just use simple system.out

--- src/com/npickard/HelloWorld.java
+++ src/com/npickard/HelloWorld.java
@@ -3,8 +3,7 @@
  */
 package com.npickard;
 
-import org.apache.log4j.BasicConfigurator;
-import org.apache.log4j.Logger;
+
 
 /**
  * @author nigel
@@ -13,15 +12,8 @@ import org.apache.log4j.Logger;
  */
 public class HelloWorld {
 
-	// Define a static logger variable so that it references the
-	// Logger instance named "MyApp".
-	static Logger logger = Logger.getLogger(HelloWorld.class);
-
 	public static void main(String[] args) {
 
-		// Set up a simple configuration that logs on the console.
-		BasicConfigurator.configure();
-
-		logger.info("Hello World!");
+		System.out.println("Hello World!");
 	}
 }
